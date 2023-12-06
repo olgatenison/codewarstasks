8814b7d42abcd34c95f0cfce0194b6952f00b9fc
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-12-07 00:01:45 +0200

    second

--- js/01.js
+++ js/01.js
@@ -23,3 +23,28 @@ function isPangram(string) {
 
 const прикладСтрічки = "Грає жвавий пес на цьому березі річки";
 console.log(isPangram(прикладСтрічки)); // Вивід: false
+
+// 2
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+  const averageClassScore =
+    classPoints.reduce((sum, score) => sum + score, 0) / classPoints.length;
+
+  return yourPoints > averageClassScore;
+}
+
+// Приклад використання:
+// const classPoints = [75, 80, 92, 88, 78];
+// const yourPoints = 85;
+
+// const result = betterThanAverage(classPoints, yourPoints);
+// console.log(result);
