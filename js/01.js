62f667585eb85c046f9f69d3490f6477e4c38f91
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-01-10 22:10:12 +0200

    pin check

--- js/01.js
+++ js/01.js
@@ -59,3 +59,22 @@ function doubleChar(str) {
     .map((char) => char + char)
     .join("");
 }
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+// function validatePIN(pin) {
+//   if (pin.length === 4 || pin.length === 6) {
+//     return /^\d+$/.test(pin); // Проверяем, состоит ли pin только из цифр
+//   }
+//   return false; // Возвращаем false для PIN с неправильной длиной
+// }
+function validatePIN(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+}
