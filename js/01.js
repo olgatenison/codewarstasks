40c71f43416d5e40aeddc8578ca4c19aae9ba16d
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-02-04 23:39:16 +0200

    dna

--- js/01.js
+++ js/01.js
@@ -78,3 +78,39 @@ function doubleChar(str) {
 function validatePIN(pin) {
   return /^(\d{4}|\d{6})$/.test(pin);
 }
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+function dnaStrand(dna) {
+  let complementaryStrand = "";
+  for (let base of dna) {
+    switch (base) {
+      case "A":
+        complementaryStrand += "T";
+        break;
+      case "T":
+        complementaryStrand += "A";
+        break;
+      case "C":
+        complementaryStrand += "G";
+        break;
+      case "G":
+        complementaryStrand += "C";
+        break;
+      default:
+        // Handle any other characters if needed
+        complementaryStrand += base;
+    }
+  }
+  return complementaryStrand;
+}
